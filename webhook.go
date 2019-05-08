81889c49974f13d263a17980bf10ef72389c1bb4
Mariusz Gronczewski <xani666@gmail.com>
2019-05-08 19:06:16 +0200

    drop dependency, it does silly stuff like loading templates without asking

--- webhook.go
+++ webhook.go
@@ -1,29 +1,44 @@
 package mon
 
 import (
+	"encoding/json"
 	"net/http"
-	"gopkg.in/unrolled/render.v1"
 )
-var r = render.New()
 
 // HandleMetrics is basic web hook that returns JSON dump of metrics in GlobalRegistry
 func HandleMetrics( w http.ResponseWriter, req *http.Request) {
-	r.JSON(w, http.StatusOK,  GlobalRegistry.GetRegistry())
+	js, err := json.Marshal(GlobalRegistry.GetRegistry())
+	if err != nil {
+		w.Write([]byte(`{"msg":"JSON marshalling error"}`))
+	} else {
+		w.Write(js)
+	}
 
 }
 // HandleHealthchecks returns GlobalStatus with appropriate HTTP code
 func HandleHealthcheck ( w http.ResponseWriter, req *http.Request) {
+	var httpStatus int
+	w.Header().Set("Content-Type", "application/json")
 	switch GlobalStatus.State {
 	case StateOk:
-		r.JSON(w, http.StatusOK,  GlobalStatus)
+		httpStatus =  http.StatusOK
 	case StateWarning:
-		r.JSON(w, http.StatusOK,  GlobalStatus)
+		httpStatus =  http.StatusOK
 	case StateUnknown:
-		r.JSON(w, http.StatusInternalServerError,  GlobalStatus)
+		httpStatus =  http.StatusInternalServerError
 	case StateInvalid:
-		r.JSON(w, http.StatusInternalServerError,  GlobalStatus)
+		httpStatus =  http.StatusInternalServerError
 	default:
-		r.JSON(w, http.StatusServiceUnavailable,  GlobalStatus)
+		httpStatus =  http.StatusServiceUnavailable
 	}
+	js, err := json.Marshal(GlobalStatus)
+
+	if httpStatus != http.StatusOK {
+		http.Error(w, "server error", httpStatus)
+	} else if err != nil {
+		http.Error(w, err.Error(), httpStatus)
+	}
+	w.Write(js)
+
 
 }
