e7fae1da7d6481101366d7aa17f9e06b710e9841
Mariusz Gronczewski <xani666@gmail.com>
2022-02-28 15:43:21 +0100

    rewrite for testify

--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/efigence/go-mon
 
+go 1.16
+
 require (
 	github.com/efigence/go-libs v0.0.0-20171115061947-5b2936a0c2a1
 	github.com/smartystreets/goconvey v1.6.4
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- status_test.go
+++ status_test.go
@@ -1,7 +1,6 @@
 package mon
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -14,77 +13,65 @@ func TestSummaryMessage(t *testing.T) {
 	c4, _ := s.NewComponent("backup")
 	c5, _ := s.NewComponent("transcoder")
 
+	assert.NoError(t, c1.Update(StateOk, "msg1"))
+	assert.NoError(t, c2.Update(StateWarning, "msg2"))
+	assert.NoError(t, c3.Update(StateCritical, "msg3"))
+	assert.NoError(t, c4.Update(StateUnknown, "msg4"))
+	assert.Error(t, c5.Update(123, "msg5"))
 
-	assert.NoError(t,c1.Update(StateOk, "msg1"))
-	assert.NoError(t,c2.Update(StateWarning, "msg2"))
-	assert.NoError(t,c3.Update(StateCritical, "msg3"))
-	assert.NoError(t,c4.Update(StateUnknown, "msg4"))
-	assert.Error(t,c5.Update(123, "msg5"))
-
-	assert.Contains(t,s.GetMessage(), "msg1")
-	assert.Contains(t,s.GetMessage(), "msg2")
-	assert.Contains(t,s.GetMessage(), "msg3")
-	assert.Contains(t,s.GetMessage(), "msg4")
-	assert.NotContains(t,s.GetMessage(), "msg5")
-	assert.Equal(t,s.GetState() ,StateCritical)
+	assert.Contains(t, s.GetMessage(), "msg1")
+	assert.Contains(t, s.GetMessage(), "msg2")
+	assert.Contains(t, s.GetMessage(), "msg3")
+	assert.Contains(t, s.GetMessage(), "msg4")
+	assert.NotContains(t, s.GetMessage(), "msg5")
+	assert.Equal(t, s.GetState(), StateCritical)
 
 }
 
 func TestSummaryState(t *testing.T) {
 	s := NewStatus("testStatus")
 	c1, _ := s.NewComponent("db")
-	Convey("After-init state unknown", t, func() {
-		So(s.GetState(),ShouldNotEqual,StateOk)
-		So(s.GetState(),ShouldEqual,StateUnknown)
-		So(c1.GetState(),ShouldNotEqual,StateOk)
-		So(c1.GetState(),ShouldEqual,StateUnknown)
+	t.Run("After-init state unknown", func(t *testing.T) {
+		assert.NotEqual(t, s.GetState(), StateOk)
+		assert.Equal(t, s.GetState(), StateUnknown)
+		assert.NotEqual(t, c1.GetState(), StateOk)
+		assert.Equal(t, c1.GetState(), StateUnknown)
 	})
 	c2, _ := s.NewComponent("storage")
 	c3, _ := s.NewComponent("kk")
 	c2.Update(StateCritical, "bad things happened")
 	c3.Update(StateOk, "bad things happened")
-	Convey("Should pick most dangerous state of all subservices", t, func() {
-		So(s.GetState(),ShouldEqual,StateCritical)
-		So(c2.GetState(),ShouldEqual,StateCritical)
-		So(c3.GetState(),ShouldEqual,StateOk)
+	t.Run("Should pick most dangerous state of all subservices", func(t *testing.T) {
+		assert.Equal(t, s.GetState(), StateCritical)
+		assert.Equal(t, c2.GetState(), StateCritical)
+		assert.Equal(t, c3.GetState(), StateOk)
 	})
 }
 
 func TestCreation(t *testing.T) {
-	s := NewStatus("testStatus","with long name","and description")
+	s := NewStatus("testStatus", "with long name", "and description")
 	_, err1 := s.NewComponent("db")
-	Convey("Create status with component",t,func() {
-		So(err1,ShouldBeNil)
-	})
+	assert.Nil(t, err1)
 	_, err2 := s.NewComponent("db")
-	Convey("do not allow double create",t,func() {
-		So(err2,ShouldNotBeNil)
-	})
+	assert.Error(t, err2, "do not allow double create")
 }
 func TestBadInput(t *testing.T) {
-	s := NewStatus("testStatus","with long name","and description")
-	c1 := s.MustNewComponent("db","some db")
+	s := NewStatus("testStatus", "with long name", "and description")
+	c1 := s.MustNewComponent("db", "some db")
 	err3 := s.Update(StateOk, "some message")
-	Convey("Do not allow updating status with children",t,func() {
-		So(err3,ShouldNotBeNil)
-	})
-	err4 := c1.Update(234,"badState")
-	Convey("Do not allow updating with state code out of range",t,func() {
-		So(err4,ShouldNotBeNil)
-	})
-	assert.Panics(t, func () {s.MustUpdate(222,"test")})
-	assert.Panics(t, func () {s.MustNewComponent("db")})
+	assert.Error(t, err3, "do not allow updating parent that has children")
+	err4 := c1.Update(234, "badState")
+	assert.Error(t, err4, "Do not allow updating with state code out of range")
+	assert.Panics(t, func() { s.MustUpdate(222, "test") })
+	assert.Panics(t, func() { s.MustNewComponent("db") })
 
 }
 
 func TestFormatters(t *testing.T) {
-	s := NewStatus("testStatus","with long name","and description")
-	c1, _ := s.NewComponent("db","some db")
+	s := NewStatus("testStatus", "with long name", "and description")
+	c1, _ := s.NewComponent("db", "some db")
 	c1.Update(StateCritical, "some message")
 	_ = c1
 	out := s.GetMessage()
-	Convey("no separators between empty types",t,func() {
-		So(out,ShouldNotContainSubstring,"=#=")
-	})
-
+	assert.NotContains(t, out, "=#=")
 }
